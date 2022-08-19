8dee8e805193813c96c106b507e66fedc0de1729
dkingbrandt <dkingbrandt@hotmail.com>
2022-08-19 18:04:18 +0200

    api commit

--- Api/serviceAPI.js
+++ Api/serviceAPI.js
@@ -40,4 +40,57 @@ router.post("/myPage",(request, response)=>{
     });
 })
 
+router.put("/myPage/:serviceid", (request, response) => {
+
+    const serviceid = Number(request.params.serviceid);
+    const heading = request.body.heading;
+    const description = request.body.description;
+    const img = request.body.img;
+    const email = request.body.email;
+
+    const newService ={
+        serviceid,
+        heading,
+        description,
+        img,
+        email,
+    
+    
+   
+    }
+  
+
+    const serviceIndex = services.findIndex((service)=>service.id === serviceid) // hämtar ut index för todo vi vill uppdatera
+    services[serviceIndex] = newService                                  // i det index vi hämtar ut vill vi skriva över med newTodo
+    
+
+
+
+   response.json({             // skickar tillbaka svar .status- success, metod -put 
+    status: 'success',
+    method: request.method,
+    data: newService,
+  });
+    
+})
+
+router.delete('/myPage/:serviceid', (request, response) => {
+    
+    const serviceid = Number(request.params.serviceid)
+    
+     const serviceIndex = services.findIndex((service)=>service.id === serviceid) 
+                                   
+    
+    if (serviceIndex > -1) {
+      services.splice(serviceIndex, 1)
+    }
+  
+  
+  response.json({             // skickar tillbaka svar .status- success, metod -put 
+    status: 'success',
+    method: request.method,
+    data: serviceid,
+  });
+})  
+
 module.exports = router;
