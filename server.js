e41463a5da358322962a8c9e83f6e5e111987694
BexRyd <becka_91_@hotmail.com>
2022-08-26 14:59:42 +0100

    jwt

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const dotenv = require('dotenv');
 const bodyParser = require("body-parser");
 const routerServices = require("./Api/serviceAPI")
 const routerSignUp = require("./Api/SignUpAPI")
 const cors = require("cors")
 const db = require("./database/db")
 
-
+dotenv.config({ path: './config.env' });
 const app = express();
 const port = 8080;
 
