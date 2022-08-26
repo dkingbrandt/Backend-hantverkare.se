103aed6dc690f70fc878c84189edaf09099527d2
BexRyd <becka_91_@hotmail.com>
2022-08-26 13:33:20 +0100

    signup first commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routerServices = require("./Api/serviceAPI")
+const routerSignUp = require("./Api/SignUpAPI")
 const cors = require("cors")
 const db = require("./database/db")
 
@@ -15,10 +16,12 @@ app.use(cors({
     credentials: true
 }))
 
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-
+app.use(routerSignUp);
 app.use(routerServices);
+
 app.listen(8080, () => {
     console.log(`listening on port ${port}`);
 })
