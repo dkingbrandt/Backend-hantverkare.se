12d05f27a92210abdc9e499474085537fbacbe10
BexRyd <becka_91_@hotmail.com>
2022-08-29 15:16:07 +0100

    user+jwt

--- Api/SignUpAPI.js
+++ Api/SignUpAPI.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controller/authController")
+const userController = require("../controller/userController")
 
 
 router.post("/SignUp", authController.signup,(request, response, next) => {
+
+  response.json({
+    status: "success",
+    method: request.method,
+    
+  })
+});
+router.post("/login", authController.login,(request, response, next) => {
+
+  response.json({
+    status: "success",
+    method: request.method,
+    
+  })
+});
+router.get("/users", userController.getAllUsers,(request, response, next) => {
 
   response.json({
     status: "success",
