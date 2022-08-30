2b6d6e0a9410dafdb8a59eae6a8ecbf22098a3d3
dkingbrandt <dkingbrandt@hotmail.com>
2022-08-30 17:58:06 +0200

    commit on inlogg and docker

--- controller/authController.js
+++ controller/authController.js
@@ -3,54 +3,54 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utility/catchAsync');
 
 
-const signToken = id =>{
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn:process.env.JWT_EXPIRES_IN
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
     })
 }
 
-exports.signup = catchAsync(async(req,res, next)=>{
-const newUser = await User.create({
-   name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm
+exports.signup = catchAsync(async (req, res, next) => {
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
     })
 
     const token = signToken(newUser._id);
 
-    
+
 
 
     res.status(201).json({
         status: "success",
         token,
-        data:{
-            user:newUser
+        data: {
+            user: newUser
         }
     })
 })
 
-exports.login = catchAsync(async (req, res, next)=>{
-    const {email, password} = req.body;
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
 
-    if(!email || !password){
-         return res.status(400).json({
-      success: false,
-      error: " ERROR, email or password missing",
-    })
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            error: " ERROR, email or password missing",
+        })
     }
- // check if user excists && password is correct
-    const user = await User.findOne({email}).select("+password") //use select to get back password to input
+    // check if user excists && password is correct
+    const user = await User.findOne({ email }).select("+password") //use select to get back password to input
 
-    
-    
-    if(!user || !(await user.correctPassword(password, user.password))){
+
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
         return res.status(401).json({
             success: false,
             error: "incorrect email or password"
         })
-        
+
     }
     console.log(user);
     const token = signToken(user._id);
@@ -61,3 +61,21 @@ exports.login = catchAsync(async (req, res, next)=>{
 
 
 })
+
+exports.protect = catchAsync(async (req, res, next) => {
+    let token;
+    if (
+        req.headers.authorization && req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1];
+
+    }
+    console.log(token);
+
+    if (!token) {
+        return res.status(401).json({ success: false, error: "you are not logged in" })
+    }
+
+
+    next()
+});
