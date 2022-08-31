a2eaf3cacac20ebcef24c70de244e8224869b537
BexRyd <becka_91_@hotmail.com>
2022-08-31 15:00:39 +0100

    delete function

--- controller/userController.js
+++ controller/userController.js
@@ -33,17 +33,16 @@ exports.getAllUsers = catchAsync(async (req,res, next)=>{
     })
  }
  exports.deleteUser = async(req,res)=>{
-    await User.findByIdAndDelete({id:req.params.userId}, (err, user)=>{
-        if(err){
-            return res.status(400).json({ success: false, error: err});
-        }
-
-        if(!user){
-            return res.status(404).json({ success: false, error: "User not found"});
-        }
+    try{await User.findByIdAndDelete({_id:req.params.userId}, (err, user)=>{
+        
 
         return res.status(200).json({success: true, data: user});
-    })
-    .clone()
-    .catch((err)=> console.log(err));
+        
+    }
+   ) .clone()}
+    catch(err){
+     return res.status(404).json({ success: false, error: [err, "user not found"]})
+    }
+    
+   
  }
