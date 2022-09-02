496be06b88a160127b599d991d2ae39f29d52871
BexRyd <becka_91_@hotmail.com>
2022-09-02 16:02:55 +0100

    done

--- models/userModel.js
+++ models/userModel.js
@@ -58,6 +58,13 @@ userSchema.pre("save", async function(next){
     next();
 })
 
+userSchema.pre("save", function(next){
+    if(!this.isModified("password") || this.isNew)return next();
+    this.passwordChangedAt = Date.now() -1000; //-1000 just in case it takes to long
+    next();
+
+})
+
 userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
     return  await bcrypt.compare(candidatePassword, userPassword);
 }
