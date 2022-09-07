d2a5b570a3f3e99c55eb41ee9e1a0993b6a1d5f7
dkingbrandt <dkingbrandt@hotmail.com>
2022-09-08 00:10:33 +0200

    make it better

--- server.js
+++ server.js
@@ -13,22 +13,22 @@ const port = 8080;
 app.use(bodyParser.json());
 
 app.use(cors({
-    orgin: "http://127.0.0.1:3000",
-    credentials: true
+  orgin: "http://127.0.0.1:3000",
+  credentials: true
 }))
 
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    console.log(req.headers);
-    next();
+  req.requestTime = new Date().toISOString();
+  console.log(req.headers);
+  next();
 });
 
 app.use(routerSignUp);
 app.use(routerServices);
 
 app.listen(8080, () => {
-    console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 })
